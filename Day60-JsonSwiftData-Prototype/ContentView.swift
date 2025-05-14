935c80badbf3ea950d3c81ec692f10ce600ad67f
RobertHxC <california.beatdown@icloud.com>
2025-05-14 12:34:03 -0700

    Initial Commit

---
+++ Day60-JsonSwiftData-Prototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day60-JsonSwiftData-Prototype
+//
+//  Created by Robert Estrada on 5/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day60-JsonSwiftData-Prototype/Day60_JsonSwiftData_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60_JsonSwiftData_PrototypeApp.swift
+//  Day60-JsonSwiftData-Prototype
+//
+//  Created by Robert Estrada on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Day60_JsonSwiftData_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
